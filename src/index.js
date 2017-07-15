3a059b01cb97425bc411f0b14eb73ea41e4c664e
Amanpreet Singh <apsdehal@gmail.com>
2017-07-15 16:52:18 -0700

    Add material ui to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
