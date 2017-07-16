f55de606776e072a79d772d92496ac07f15a8dae
Amanpreet Singh <apsdehal@gmail.com>
2017-07-15 23:37:28 -0700

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const constants = {
+  IMAGE_ID_IDENTIFIER: 'imageId',
+  IS_BOARD_IMAGE_IDENTIFIER: 'isBlock',
+  IMAGE_PATH_IDENTIFIER: 'imagePath',
+  CERTIFY_IMAGE_STATEMENT: 'I certify that I have the rights to use this image in an open source project',
+  NOT_CERTIFIED_WARNING: 'Certification is necessary to move forward',
+  BOARD_IMAGE_TOGGLE_LABEL: 'Is this a board image?',
+  IMAGE_ID_FLOATING_LABEL: 'Enter image id',
+  IMAGE_ID_HINT_TEXT: 'Image id'
+}
+
+export default constants;
