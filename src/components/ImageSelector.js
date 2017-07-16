7464b3744dfdd0190acadd5ff5ebd759ce0cb041
Amanpreet Singh <apsdehal@gmail.com>
2017-07-15 23:38:01 -0700

    Add component for image selector with css

---
+++ src/components/ImageSelector.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Toggle from 'material-ui/Toggle';
+import TextField from 'material-ui/TextField';
+import constants from '../constants';
+import styles from '../styles';
+
+import './ImageSelector.css';
+
+const ImageSelector = (props) => {
+   return (
+     <div style={styles.block} className="image-selector-div">
+      <div>
+         <RaisedButton containerElement="label" label={props.label}
+         onChange={e => {
+           props.handleChange(constants.IMAGE_PATH_IDENTIFIER, e)}}>
+            <input type="file"  style={{ display: 'none' }}/>
+         </RaisedButton>
+      </div>
+      <div>
+        <TextField
+          floatingLabelText={constants.IMAGE_ID_FLOATING_LABEL}
+          floatingLabelFixed={true}
+          hintText={constants.IMAGE_ID_HINT_TEXT}
+          errorText={props.imageIdErrorText}
+          onChange={(e, newValue) => {
+            props.handleChange(constants.IMAGE_ID_IDENTIFIER, e, newValue)}}/>
+      </div>
+      <div>
+        <Toggle
+          label={constants.BOARD_IMAGE_TOGGLE_LABEL}
+          onToggle={(e, newValue) => {
+            props.handleChange(constants.IS_BOARD_IMAGE_IDENTIFIER, e, newValue)}}/>
+      </div>
+     </div>
+   );
+}
+
+export default ImageSelector;
