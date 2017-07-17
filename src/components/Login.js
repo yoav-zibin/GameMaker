79e46dd8e4db71996525585c784f7a0aa77124ad
Amanpreet Singh <apsdehal@gmail.com>
2017-07-17 01:32:51 -0700

    Style login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { auth } from '../firebase';
 import Redirect from 'react-router-dom/Redirect';
 import Card from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const raisedButtonStyle = {
+  margin: 16
+};
+
+const attentionTextStyle = {
+  paddingTop: 20
+};
 
 class Login extends React.Component {
   state = {
@@ -28,12 +38,22 @@ class Login extends React.Component {
             <Redirect to={from || '/protected'}/>
           )}
           {from && (
-            <p>You must log in to view the page at {from.pathname}</p>
+            <p style={attentionTextStyle}>You must log in to view the page at {from.pathname}</p>
           )}
           <form onSubmit={this.handleSubmit}>
-            <input type="text" value={this.state.email} onChange={e => this.setState({email: e.target.value})} />
-            <input type="password" value={this.state.password} onChange={e => this.setState({password: e.target.value})} />
-            <button type="submit">Sign In</button>
+            <TextField
+              value={this.state.email}
+              errorText=""
+              floatingLabelText="Email"
+              onChange={e => this.setState({email: e.target.value})}
+            /><br/>
+            <TextField
+              value={this.state.password}
+              errorText=""
+              floatingLabelText="Password"
+              onChange={e => this.setState({password: e.target.value})}
+            /><br/>
+            <RaisedButton label="Sign In" primary={true} onClick={this.handleSubmit} style={raisedButtonStyle} />
           </form>
         </Card>
       </div>
