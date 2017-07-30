9a4b7e1a03489b6f04942f7de1eb381bc4f556db
Amanpreet Singh <apsdehal@gmail.com>
2017-07-29 20:59:54 -0700

    Add a container for spec builder and add drop context

--- src/components/GameSpecBuilder.js
+++ src/components/GameSpecBuilder.js
@@ -1,144 +1,18 @@
 import React from 'react';
+import { DragDropContextProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
-import styles from '../styles';
-import { boardImagesDbRef, otherImagesDbRef } from '../firebase';
-import GridListCustom from './GridListCustom';
-
-
-import RaisedButton from 'material-ui/RaisedButton';
-import FlatButton from 'material-ui/FlatButton';
-import {
-  Step,
-  Stepper,
-  StepLabel,
-} from 'material-ui/Stepper';
+import Board from './gamespec/Board';
 
 class GameSpecBuilder extends React.Component {
-  state = {
-    boardImages: [],
-    otherImages: [],
-    selectedBoard: "",
-    stepIndex: 0,
-    finished: false
-  };
-
-  componentDidMount() {
-    let that = this;
-    boardImagesDbRef.once('value').then(function (data) {
-      that.setState({
-        boardImages: data.val()
-      });
-    });
-
-    otherImagesDbRef.once('value').then(function (data) {
-      that.setState({
-        otherImages: data.val()
-      });
-    });
-  }
-
-  updateStepIndex(stepIndex) {
-    this.setState({
-      stepIndex: stepIndex + 1,
-      finished: stepIndex >= 1
-    });
-  }
-
-  handleGridTileClickBoard(key) {
-    this.setState({
-      selectedBoard: key
-    });
-  }
-
-  getStepContent(stepIndex) {
-    switch (stepIndex) {
-      case 0: {
-        return (
-          <GridListCustom
-            handleGridTileClick={this.handleGridTileClickBoard.bind(this)}
-            data={this.state.boardImages}
-            selectedKey={this.state.selectedBoard}/>
-        );
-      }
-
-      case 1: {
-        return (
-          <GridListCustom
-            handleGridTileClick={() => {}}
-            data={this.state.otherImages}
-            selectedKey={this.state.selectedBoard}/>
-        );
-      }
-      default: {
-        break;
-      }
-    }
-  }
-
-  handlePrev = () => {
-    const { stepIndex } = this.state;
-    if (stepIndex > 0) {
-      this.setState({stepIndex: stepIndex - 1});
-    }
-  };
-
-  handleNext = () => {
-    const { stepIndex } = this.state;
-    if (stepIndex === 0) {
-      if (!this.state.selectedBoard.length) {
-        return;
-      }
-      this.updateStepIndex(stepIndex);
-    } else {
-      this.updateStepIndex(stepIndex);
-    }
-  }
-
   render() {
-    const { stepIndex, finished } = this.state;
     return (
-      <div style={styles.container}>
-        <Stepper activeStep={stepIndex}>
-          <Step>
-            <StepLabel>Select the board</StepLabel>
-          </Step>
-          <Step>
-            <StepLabel>Build game specification</StepLabel>
-          </Step>
-          <Step>
-            <StepLabel>Check generated spec</StepLabel>
-          </Step>
-        </Stepper>
-        <div style={styles.content}>
-          {finished ? (
-            <div>
-                <RaisedButton label="Reset" primary={true}
-                  onClick={(event) => {
-                    event.preventDefault();;
-                    this.setState({stepIndex: 0, finished: false});
-                  }}/>
-            </div>
-          ) : (
-            <div>
-              <div>{this.getStepContent(stepIndex)}</div>
-              <div style={{marginTop: 12}}>
-                <FlatButton
-                  label="Back"
-                  disabled={stepIndex === 0}
-                  onTouchTap={this.handlePrev}
-                  style={{marginRight: 12}}
-                />
-                <RaisedButton
-                  label={stepIndex === 1 ? 'Upload' : 'Next'}
-                  primary={true}
-                  onTouchTap={this.handleNext}
-                />
-              </div>
-            </div>
-          )}
+      <DragDropContextProvider backend={HTML5Backend}>
+        <div>
+          <Board boardImage={this.props.boardImage}/>
         </div>
-      </div>
-    )
+      </DragDropContextProvider>
+    );
   }
 }
 
---
+++ src/components/GameSpecBuilderContainer.js
@@ -0,0 +1,145 @@
+import React from 'react';
+
+import styles from '../styles';
+import { boardImagesDbRef, otherImagesDbRef } from '../firebase';
+import BoardList from './gamespec/BoardList';
+import GameSpecBuilder from './GameSpecBuilder';
+
+
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import {
+  Step,
+  Stepper,
+  StepLabel,
+} from 'material-ui/Stepper';
+
+class GameSpecBuilderComponent extends React.Component {
+  state = {
+    boardImages: [],
+    otherImages: [],
+    selectedBoard: "",
+    stepIndex: 0,
+    finished: false
+  };
+
+  componentDidMount() {
+    let that = this;
+    boardImagesDbRef.once('value').then(function (data) {
+      that.setState({
+        boardImages: data.val()
+      });
+    });
+
+    otherImagesDbRef.once('value').then(function (data) {
+      that.setState({
+        otherImages: data.val()
+      });
+    });
+  }
+
+  updateStepIndex(stepIndex) {
+    this.setState({
+      stepIndex: stepIndex + 1,
+      finished: stepIndex >= 1
+    });
+  }
+
+  handleGridTileClickBoard(key) {
+    this.setState({
+      selectedBoard: key
+    });
+  }
+
+  getStepContent(stepIndex) {
+    switch (stepIndex) {
+      case 0: {
+        return (
+          <BoardList
+            handleGridTileClick={this.handleGridTileClickBoard.bind(this)}
+            data={this.state.boardImages}
+            selectedKey={this.state.selectedBoard}/>
+        );
+      }
+
+      case 1: {
+        return (
+          <GameSpecBuilder
+            images={this.state.otherImages}
+            boardImage={this.state.boardImages[this.state.selectedBoard]}/>
+        );
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
+  handlePrev = () => {
+    const { stepIndex } = this.state;
+    if (stepIndex > 0) {
+      this.setState({stepIndex: stepIndex - 1});
+    }
+  };
+
+  handleNext = () => {
+    const { stepIndex } = this.state;
+    if (stepIndex === 0) {
+      if (!this.state.selectedBoard.length) {
+        return;
+      }
+      this.updateStepIndex(stepIndex);
+    } else {
+      this.updateStepIndex(stepIndex);
+    }
+  }
+
+  render() {
+    const { stepIndex, finished } = this.state;
+    return (
+      <div style={styles.container}>
+        <Stepper activeStep={stepIndex}>
+          <Step>
+            <StepLabel>Select the board</StepLabel>
+          </Step>
+          <Step>
+            <StepLabel>Build game specification</StepLabel>
+          </Step>
+          <Step>
+            <StepLabel>Check generated spec</StepLabel>
+          </Step>
+        </Stepper>
+        <div style={styles.content}>
+          {finished ? (
+            <div>
+                <RaisedButton label="Reset" primary={true}
+                  onClick={(event) => {
+                    event.preventDefault();;
+                    this.setState({stepIndex: 0, finished: false});
+                  }}/>
+            </div>
+          ) : (
+            <div>
+              <div>{this.getStepContent(stepIndex)}</div>
+              <div style={{marginTop: 12}}>
+                <FlatButton
+                  label="Back"
+                  disabled={stepIndex === 0}
+                  onTouchTap={this.handlePrev}
+                  style={{marginRight: 12}}
+                />
+                <RaisedButton
+                  label={stepIndex === 1 ? 'Upload' : 'Next'}
+                  primary={true}
+                  onTouchTap={this.handleNext}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GameSpecBuilderComponent;
