a44f146392838b7f7adf77e932e05e77e8717693
Siqi Ma <sm7046@nyu.edu>
2017-10-30 19:50:58 -0400

    make dice and toggable element can be played on board

--- src/components/gamespec/CanvasImage.js
+++ src/components/gamespec/CanvasImage.js
@@ -6,7 +6,24 @@ class CanvasImage extends React.Component {
   state = {
     image: null
   };
+
   componentDidMount() {
+    this.setImage();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.image !== nextProps) {
+      const image = new window.Image();
+      image.src = this.props.src;
+      image.onload = () => {
+        this.setState({
+          image: image
+        });
+      };
+    }
+  }
+
+  setImage = () => {
     const image = new window.Image();
     image.src = this.props.src;
     image.onload = () => {
@@ -14,7 +31,7 @@ class CanvasImage extends React.Component {
         image: image
       });
     };
-  }
+  };
 
   render() {
     return <Image ref="image" {...this.props} image={this.state.image} />;
