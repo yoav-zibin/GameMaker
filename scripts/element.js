66b6e8b3b8b8ec0452590d71ff230df9b6aa5933
Yangyan Luo <yl4308@nyu.edu>
2017-10-30 23:26:56 -0400

    Add scripts for migrating DB schema

---
+++ scripts/element.js
@@ -0,0 +1,50 @@
+const firebase = require('firebase');
+const request = require('request');
+
+import config from '../src/config.json';
+
+const firebaseApp = firebase.initializeApp(config);
+const db = firebaseApp.database();
+const auth = firebaseApp.auth();
+
+let email = 'youremail';
+let password = 'yourpassword';
+firebase
+  .auth()
+  .signInWithEmailAndPassword(email, password)
+  .then(function(user) {
+    console.log('uid', user.uid);
+
+    const ref = db.ref('gameBuilder/elements');
+
+    var fs = require('fs');
+    var file = 'universalgamemaker-export.json';
+    var result = JSON.parse(fs.readFileSync(file));
+    var images = result['images'];
+
+    for (var image in images) {
+      if (images[image]['uploader_email'] === undefined) continue;
+      let key = image;
+      let value = {};
+      let image_list = [];
+
+      image_list.push({ imageId: image });
+      value['uploaderEmail'] = images[image]['uploader_email'];
+      value['uploaderUid'] = user.uid;
+      value['width'] = images[image]['width'];
+      value['height'] = images[image]['height'];
+      value['createdOn'] = firebase.database.ServerValue.TIMESTAMP;
+      value['isDraggable'] = false;
+      value['isDrawable'] = false;
+      value['rotatableDegrees'] = 1;
+      value['elementKind'] = 'standard';
+      value['images'] = image_list;
+
+      let childkey = ref.push().key;
+      ref.child(childkey).set(value);
+      console.log(childkey);
+    }
+  })
+  .catch(function(error) {
+    //Handle error
+  });
---
+++ scripts/images.js
@@ -0,0 +1,60 @@
+const firebase = require('firebase');
+const request = require('request');
+require('firebase/storage');
+
+import config from '../src/config.json';
+
+const firebaseApp = firebase.initializeApp(config);
+const db = firebaseApp.database();
+const auth = firebaseApp.auth();
+const storageRef = firebaseApp.storage().ref();
+
+let email = 'youremail';
+let password = 'yourpassword';
+firebase
+  .auth()
+  .signInWithEmailAndPassword(email, password)
+  .then(function(user) {
+    console.log('uid', user.uid);
+
+    const ref = db.ref('gameBuilder/images');
+
+    var fs = require('fs');
+    var file = 'universalgamemaker-export.json';
+    var result = JSON.parse(fs.readFileSync(file));
+    var images = result['images'];
+    let i = 0;
+
+    for (var image in images) {
+      if (images[image]['uploader_email'] === undefined) continue;
+      let key = image;
+      let value = {};
+      value['uploaderEmail'] = images[image]['uploader_email'];
+      value['uploaderUid'] = user.uid;
+      value['width'] = images[image]['width'];
+      value['height'] = images[image]['height'];
+      value['isBoardImage'] = images[image]['is_board_image'];
+      value['downloadURL'] = images[image]['downloadURL'];
+      value['name'] = images[image]['name'];
+      let filename = images[image]['downloadURL']
+        .split('images%2F')[1]
+        .split('?')[0];
+      value['cloudStoragePath'] = 'images/' + filename;
+      value['createdOn'] = firebase.database.ServerValue.TIMESTAMP;
+
+      request(images[image]['downloadURL'], function(
+        error,
+        response,
+        body,
+        image
+      ) {
+        if (!error && response.statusCode == 200) {
+          value['sizeInBytes'] = Number(response.headers['content-length']);
+          ref.child(key).set(value);
+        }
+      });
+    }
+  })
+  .catch(function(error) {
+    //Handle error
+  });
