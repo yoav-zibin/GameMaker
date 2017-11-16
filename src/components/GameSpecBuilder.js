acf3a4d1057c7dcc1f127d1ec4b68f1e2577b536
Siqi Ma <sm7046@nyu.edu>
2017-11-16 16:25:31 -0500

    fix HTML5 bug

--- src/components/GameSpecBuilder.js
+++ src/components/GameSpecBuilder.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { DragDropContextProvider } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
+//import { DragDropContextProvider } from 'react-dnd';
+//import HTML5Backend from 'react-dnd-html5-backend';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
 
 import PieceList from './gamespec/PieceList';
 import Board from './gamespec/Board';
 import styles from '../styles';
+import withDragDropContext from './withDragDropContext';
 
 const flexStyle = {
   width: '70%',
@@ -44,34 +45,34 @@ const GameSpecBuilder = props => {
   ];
 
   return (
-    <DragDropContextProvider backend={HTML5Backend}>
-      <div style={flexStyle}>
-        <div style={flexElement}>
-          <DropDownMenu
-            value={props.getValue()}
-            onChange={(e, newValue) => {
-              props.setValue(newValue);
-            }}
-          >
-            <MenuItem value={0} primaryText="standard" />
-            <MenuItem value={1} primaryText="toggable" />
-            <MenuItem value={2} primaryText="card" />
-            <MenuItem value={3} primaryText="dice" />
-            <MenuItem value={4} primaryText="cardsDeck" />
-            <MenuItem value={5} primaryText="piecesDeck" />
-          </DropDownMenu>
-          <PieceList data={elements[props.getValue()].data} />
-        </div>
-        <Board
-          setBoardSize={setBoardSize}
-          setItems={setItems}
-          getItems={getItems}
-          boardImage={boardImage}
-          allImages={allImages}
-        />
+    //<DragDropContextProvider backend={HTML5Backend}>
+    <div style={flexStyle}>
+      <div style={flexElement}>
+        <DropDownMenu
+          value={props.getValue()}
+          onChange={(e, newValue) => {
+            props.setValue(newValue);
+          }}
+        >
+          <MenuItem value={0} primaryText="standard" />
+          <MenuItem value={1} primaryText="toggable" />
+          <MenuItem value={2} primaryText="card" />
+          <MenuItem value={3} primaryText="dice" />
+          <MenuItem value={4} primaryText="cardsDeck" />
+          <MenuItem value={5} primaryText="piecesDeck" />
+        </DropDownMenu>
+        <PieceList data={elements[props.getValue()].data} />
       </div>
-    </DragDropContextProvider>
+      <Board
+        setBoardSize={setBoardSize}
+        setItems={setItems}
+        getItems={getItems}
+        boardImage={boardImage}
+        allImages={allImages}
+      />
+    </div>
+    //</DragDropContextProvider>
   );
 };
 
-export default GameSpecBuilder;
+export default withDragDropContext(GameSpecBuilder);
