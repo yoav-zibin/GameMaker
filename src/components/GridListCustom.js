75f070af06106573618b9c52e324d768ee942353
yangyangluo <yl4308@nyu.edu>
2017-12-07 00:45:30 -0500

    fix the gamebuilder bug

--- src/components/GridListCustom.js
+++ src/components/GridListCustom.js
@@ -6,21 +6,25 @@ import styles from '../styles';
 
 function GridListCustom(Component) {
   return props => {
-    return (
-      <div style={styles.gridListContainer}>
-        <GridList cellHeight={180} style={styles.gridList}>
-          <Subheader>{props.header}</Subheader>
-          {Object.keys(props.data)
-            .reverse()
-            .map((key, index) => {
-              let tile = props.data[key];
-              return (
-                <Component {...props} image={tile} key={key} keyProp={key} />
-              );
-            })}
-        </GridList>
-      </div>
-    );
+    if (props.data !== null) {
+      return (
+        <div style={styles.gridListContainer}>
+          <GridList cellHeight={180} style={styles.gridList}>
+            <Subheader>{props.header}</Subheader>
+            {Object.keys(props.data)
+              .reverse()
+              .map((key, index) => {
+                let tile = props.data[key];
+                return (
+                  <Component {...props} image={tile} key={key} keyProp={key} />
+                );
+              })}
+          </GridList>
+        </div>
+      );
+    } else {
+      return <div style={styles.gridListContainer}> </div>;
+    }
   };
 }
 export default GridListCustom;
