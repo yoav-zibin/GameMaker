3a5c07ae3b538ede0e4621fff7167b688ca8bfac
yangyangluo <31899747+yangyangluo@users.noreply.github.com>
2017-12-07 01:49:09 -0500

    Fix my uploads section issue (#19)
    * fix the gamebuilder bug
    
    * add notificaitonof no element uploaded
    
    * add notificaitonof no element uploaded
    
    * fix piece deck drag

--- src/components/GridListCustom.js
+++ src/components/GridListCustom.js
@@ -6,21 +6,29 @@ import styles from '../styles';
 
 function GridListCustom(Component) {
   return props => {
-    return (
-      <div style={styles.gridListContainer}>
-        <GridList cellHeight={180} style={styles.gridList}>
-          <Subheader>{props.header}</Subheader>
-          {Object.keys(props.data)
-            .reverse()
-            .map((key, index) => {
-              let tile = props.data[key];
-              return (
-                <Component {...props} image={tile} key={key} keyProp={key} />
-              );
-            })}
-        </GridList>
-      </div>
-    );
+    if (props.data !== null) {
+      return (
+        <div style={styles.gridListContainer}>
+          <GridList cellHeight={180} style={styles.gridList}>
+            <Subheader>{props.header}</Subheader>
+            {Object.keys(props.data)
+              .reverse()
+              .map((key, index) => {
+                let tile = props.data[key];
+                return (
+                  <Component {...props} image={tile} key={key} keyProp={key} />
+                );
+              })}
+          </GridList>
+        </div>
+      );
+    } else {
+      return (
+        <div style={styles.gridListContainer}>
+          You didn't upload any element yet!
+        </div>
+      );
+    }
   };
 }
 export default GridListCustom;
