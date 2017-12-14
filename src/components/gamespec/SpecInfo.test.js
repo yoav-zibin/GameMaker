8c2ab920ddc534535b745fc28c1ebd085259dfb5
Siqi Ma <sm7046@nyu.edu>
2017-12-14 14:13:59 -0500

    unit tests

---
+++ src/components/gamespec/SpecInfo.test.js
@@ -0,0 +1,37 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import SpecInfo from './SpecInfo';
+
+it('should render correct value', () => {
+  const props = {
+    handleIcon50CLick: () => {},
+    handleIcon512Click: () => {},
+    handleGameIcon50: () => {},
+    gameIcon50: [],
+    gameIcon512: [],
+    setYoutube: () => {},
+    setWiki: () => {},
+    getYoutube: () => {},
+    getWiki: () => {},
+    getGameIcon50: () => {},
+    getGameIcon512: () => {}
+  };
+
+  const wrapper = shallow(
+    <SpecInfo
+      handleIcon50CLick={props.handleIcon50CLick}
+      handleIcon512Click={props.handleGameIcon512}
+      gameIcon50={props.gameIcon50}
+      gameIcon512={props.gameIcon512}
+      setYoutube={props.setYoutube}
+      setWiki={props.setWiki}
+      getYoutube={props.getYoutube}
+      getWiki={props.getWiki}
+      getGameIcon50={props.getGameIcon50}
+      getGameIcon512={props.getGameIcon512}
+    />
+  );
+
+  expect(wrapper.find('TextField').length).toBe(2);
+  expect(wrapper.find('BoardList').length).toBe(0);
+});
---
+++ src/components/navigation/Logout.test.js
@@ -0,0 +1,21 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import Logout from './Logout';
+
+it('should logout properly', () => {
+  let myMock = jest.fn();
+  const wrapper = shallow(
+    <Logout isAuthenticated={true} handleClick={myMock} />
+  );
+  const wrapper2 = shallow(
+    <Logout isAuthenticated={false} handleClick={myMock} />
+  );
+
+  expect(wrapper.find('RaisedButton').length).toBe(1);
+  wrapper
+    .find('RaisedButton')
+    .at(0)
+    .simulate('click');
+  expect(myMock.mock.calls.length).toBe(1);
+  expect(wrapper2.find('RaisedButton').length).toBe(1);
+});
